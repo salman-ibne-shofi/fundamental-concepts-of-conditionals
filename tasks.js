7d23dd11f9f8ba0d624616bc138ddcd4e5b8869c
salman-ibne-shofi <salmanibnshafi@gmail.com>
2025-02-18 23:42:48 +0600

    burger price and BMI calculator

---
+++ tasks.js
@@ -0,0 +1,21 @@
+// burger
+const burger = 600;
+
+if (burger > 500) {
+	console.log("free coke");
+} else {
+	console.log("buy a coke 30tk");
+}
+
+// BMI calculator
+const BMI = 30;
+
+if (BMI < 18.5) {
+	console.log("underweight");
+} else if (BMI >= 18.5 && BMI <= 24.9) {
+	console.log("normal");
+} else if (BMI >= 25 && BMI <= 29.9) {
+	console.log("overweight");
+} else {
+	console.log("you are obese");
+}
